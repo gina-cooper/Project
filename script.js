c9505418cd5245816e60c211b880e6aae892c06a
Gina Cooper <65682994+gina-cooper@users.noreply.github.com>
2023-05-12 16:42:16 +0000

    Module 3a

---
+++ script.js
@@ -0,0 +1,23 @@
+function addItem(){
+    var ul = document.getElementById("shopList");
+    var newItem = document.getElementById("newItem").value;
+    var li = document.createElement("div");
+    li.innerHTML = "<input type = 'checkbox' onclick = 'deleteItem();' class = 'chk' name = '" 
+        + newItem + "'>" + "<label for ='" + newItem + "' class='lbl'>&nbsp;&nbsp;" + newItem + "</label>";
+
+    ul.appendChild(li);
+}
+
+function deleteItem(){
+    var checkboxes = document.getElementsByClassName("chk");
+    var labels = document.getElementsByClassName("lbl");
+    for (var i = 0; i < checkboxes.length; i++) {
+        box = checkboxes[i];
+        label = labels[i];
+        if(box.checked)
+        {
+            box.remove();
+            label.remove();
+        }
+    }
+}
